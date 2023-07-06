bae437118162d29f74b6446873a6dd263d00aca1
eustatos <astashkinav@gmail.com>
2023-07-06 20:15:30 +0300

    expose id for Category

--- src/main/java/com/example/dataRest/config/DataRestConfig.java
+++ src/main/java/com/example/dataRest/config/DataRestConfig.java
@@ -1,5 +1,6 @@
 package com.example.dataRest.config;
 
+import com.example.dataRest.domain.Category;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ class DataRestConfiguration {
             @Override
             public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
                 config.setBasePath(basePath);
+                config.exposeIdsFor(Category.class);
             }
         };
     }
